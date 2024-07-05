74dec74cbe085dbdc7b3b113d0a8c6de14947529
AEKDA <k.david.doc@gmail.com>
2024-07-05 18:36:35 +0300

    add gqlgen for graphql and setup project

---
+++ api/graph/directives.go
@@ -0,0 +1,25 @@
+package graph
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+func LengthDirective(ctx context.Context, obj interface{}, next graphql.Resolver, min *int, max *int) (res interface{}, err error) {
+
+	str, ok := res.(string)
+	if !ok {
+		return nil, fmt.Errorf("length directive can only be applied to strings")
+	}
+
+	if max != nil && len([]rune(str)) > *max {
+		return nil, fmt.Errorf("field exceeds the maximum length of %d", max)
+	}
+	if min != nil && len([]rune(str)) < *min {
+		return nil, fmt.Errorf("field is below the minimum length of %d", min)
+	}
+
+	return next(ctx)
+}
